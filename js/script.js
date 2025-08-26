1a4d4ceb36e11433ede21f9abd922555ee12041b
Дмитро Удовіченко <dmitroudovicenko@MacBook-Pro-Dmitro.local>
2025-08-26 15:53:32 +0300

    function

--- js/script.js
+++ js/script.js
@@ -1,63 +1,69 @@
-/* Задание на урок:
+"use strict";
+// let num = 10;
+// function showFirstMasage (text) {
+//   console.log(text);
+//   let   num = 20;
+//     console.log(num);
+// }
+// showFirstMasage("Hello World!");
+// console.log(num);
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+// function calc(a, b) {
+//     return (a + b);
+// }
+// console.log(calc(4, 3));
+// console.log(calc(42, 33));
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// let calc2 = function(a, b) {
+//     return a + b;
+// }
+// console.log(calc2(5, 6));
+// console.log(calc2(50, 6));
+
+// const calc3 = (a, b) => a + b;
+// console.log(calc3(5, 6));
+// console.log(calc3(50, 6));
+
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+// convert(500, usdCurr);
+// console.log(convert(500, usdCurr));
+// console.log(convert(500, eurCurr)); 
+
+
+const usdCurr = 28;
+const dicont = 0.9;
+function convert(amount, curr) {
+    
+   return curr * amount;
+}
+
+
+function promotion(result) {
+    console.log(result*dicont);
+}
+const res = convert(500, usdCurr);
+
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return;
     }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}; 
-
-
-for (i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+    console.log("Done");
 }
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-}
-else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-}
-else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-}
-else {
-    console.log("Произошла ошибка");
-}  
-
-console.log(personalMovieDB);
-
-
+test();
