7d6daf5a572129d2a8676b155c9c9448b5a6cdf0
Dima <dmytro.udo@gmail.com>
2025-08-26 13:24:20 +0300

    цикли

--- js/script.js
+++ js/script.js
@@ -1,59 +1,118 @@
-"use strict";
+// "use strict"
 
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 0;
+// // let  num = 50
 
-// console.log(hamburger === 3 && cola  && fries); // 1 - true, 0 - false
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++
+// // }
 
-// console.log(1 && 0); // 0
-// console.log(1 && 5); // 5
-// console.log(null && 5); // null
-// console.log(0 && "Hello"); // 0
-// console.log(1 && 2 && 3); // 3
-// console.log(1 && 2 && null && 3); // null
-// console.log(1 && 2 && 3 && 0 && 4); // 0
-// console.log(1 && 2 && 3 && 4); // 4 
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);   
 
-// if (hamburger === 3  && fries === 1 && cola === 1) {
-//     console.log("Done!");
-// }   else {
-//     console.log("Not today!");
+// // for (let i = 1; i < 10; i++) {
+// //     if (i === 6) {
+// //          break;
+// //         // continue;
+// //     }
+// //     console.log(i);
+// // }
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+
+
+//     }
+
+// }
+
+// let result = '';
+// const length = 17;
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+
+// first:for (let i = 5; i <= 10; i++) { 
+//     console.log(`first level:  ${i}`);
+//     for (let j = 5; j <= 10; j++) {
+//         console.log(`second level:  ${j}`);
+//         for (let k = 5; k <= 10; k++) {
+
+//             if (k === 2) continue first;
+//             console.log(`third level:  ${k}`);
+
+//         }
+//     }
 // }
 
 
-// Тут використовуються логічні оператори:
-// && ("І") – обидві умови мають бути істинними, інакше повертає false.
-// || ("АБО") – достатньо, щоб хоча б одна умова була істинною.
-
-// if (hamburger && cola || fries && cola) {
-//     console.log("Done!");
-// } else {
-//     console.log("Not today!");
-// }   
-
-
-
-const hamburger = 3;
-const fries = 2;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger === 3 && fries  === 2 ||  cola === 2 && nuggets) {
-    console.log("Done!");
-}   else {
-    console.log("Not today!");
+// Место для первой задачи
+function firstTask() {
+    // Пишем решение вот тут
+    for(let  i= 5; i < 11;i++ ) {
+        console.log(i)
+    }
+    
+    
 }
 
-console.log(hamburger === 3 && fries  === 2 ||  cola === 2 && nuggets); // true 
+// Место для второй задачи
+function secondTask() {
+    for (let i = 20; i >= 10; i--) {
+        if (i === 13) break;
+        console.log(i)
+    }
+}
 
+// Место для третьей задачи
+function thirdTask() {
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
 
-let johnReport, alexReport, samReport, mariaReport = "done";
-console.log(johnReport || alexReport || samReport || mariaReport); // done
+// Место для четвертой задачи
 
+// Цикл, который нужно переписать:
 
-console.log(!0); // true
-console.log(!1); // false
-console.log(!!1); // true
-console.log(!!0); // false      
+while (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+   } else {
+       console.log(i);
+    }
+ }
+
+function fourthTask() {
+    // Пишем решение вот тут
+    
+    
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+    
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
