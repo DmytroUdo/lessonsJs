94aa79b10806555bfbcaeca4a0b71404282b9973
Дмитро Удовіченко <dmitroudovicenko@MacBook-Pro-Dmitro.local>
2025-08-26 14:44:08 +0300

    .gitignor_npm

--- js/script.js
+++ js/script.js
@@ -1,118 +1,52 @@
-// "use strict"
+/* Задание на урок:
 
-// // let  num = 50
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// // while (num <= 55) {
-// //     console.log(num);
-// //     num++
-// // }
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// // do {
-// //     console.log(num);
-// //     num++;
-// // }
-// // while (num < 55);   
-
-// // for (let i = 1; i < 10; i++) {
-// //     if (i === 6) {
-// //          break;
-// //         // continue;
-// //     }
-// //     console.log(i);
-// // }
-
-
-// for (let i = 0; i < 3; i++) {
-//     console.log(i);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j);
-
-
-//     }
-
-// }
-
-// let result = '';
-// const length = 17;
-// for (let i = 1; i < length; i++) {
-
-//     for (let j = 0; j < i; j++) {
-//         result += "*";
-
-//     }
-//     result += '\n';
-// }
-
-// console.log(result);
-
-
-// first:for (let i = 5; i <= 10; i++) { 
-//     console.log(`first level:  ${i}`);
-//     for (let j = 5; j <= 10; j++) {
-//         console.log(`second level:  ${j}`);
-//         for (let k = 5; k <= 10; k++) {
-
-//             if (k === 2) continue first;
-//             console.log(`third level:  ${k}`);
-
-//         }
-//     }
-// }
-
-
-// Место для первой задачи
-function firstTask() {
-    // Пишем решение вот тут
-    for(let  i= 5; i < 11;i++ ) {
-        console.log(i)
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
     }
-    
-    
-}
 
-// Место для второй задачи
-function secondTask() {
-    for (let i = 20; i >= 10; i--) {
-        if (i === 13) break;
-        console.log(i)
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
+
+
+for (i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
 }
+if (pers)
 
-// Место для третьей задачи
-function thirdTask() {
-    for (let i = 2; i <= 10; i++) {
-        if (i % 2 === 0) {
-            console.log(i);
-        }
-    }
-}
+console.log(personalMovieDB);
 
-// Место для четвертой задачи
 
-// Цикл, который нужно переписать:
-
-while (let i = 2; i <= 16; i++) {
-    if (i % 2 === 0) {
-        continue;
-   } else {
-       console.log(i);
-    }
- }
-
-function fourthTask() {
-    // Пишем решение вот тут
-    
-    
-}
-
-// Место для пятой задачи
-
-function fifthTask() {
-    const arrayOfNumbers = [];
-
-    // Пишем решение вот тут
-    
-    
-    // Не трогаем
-    return arrayOfNumbers;
-}
